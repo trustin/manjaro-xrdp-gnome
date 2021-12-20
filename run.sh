5d1beb61a8c62a56492c1afb8f2cffef9fdad87d
Trustin Lee <t@motd.kr>
2021-12-21 00:26:01 +0900

    Use the same container name for all manjaro-xrdp-* repos

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-docker stop manjaro-xrdp-gnome >/dev/null 2>&1 || true
-docker rm manjaro-xrdp-gnome >/dev/null 2>&1 || true
-docker create --name manjaro-xrdp-gnome \
+docker stop manjaro-xrdp >/dev/null 2>&1 || true
+docker rm manjaro-xrdp >/dev/null 2>&1 || true
+docker create --name manjaro-xrdp \
   --privileged \
   --env "PUID=$(id -u)" \
   --env "PUSER=$(id -un)" \
@@ -11,4 +11,4 @@ docker create --name manjaro-xrdp-gnome \
   --publish 2222:22 \
   --shm-size 1G \
   ghcr.io/trustin/manjaro-xrdp-gnome:latest
-docker start manjaro-xrdp-gnome
+docker start manjaro-xrdp
